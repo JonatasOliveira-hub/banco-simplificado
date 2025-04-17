d5b56c7c43ec690f3b609874c738dba5871019a0
Jonatas Oliveira Silva <JonatasOliveira@ibm.com>
2025-04-17 14:40:48 -0300

    Alteração de nomes e add gitIgnore.

---
+++ pagamento-simplificado/pagamento-simplificado/src/test/java/com/banco/pagamento_simplificado/PagamentoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.pagamento_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
