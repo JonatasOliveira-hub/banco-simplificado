159c076c047c2b076a2c1239655c8e76ac99c849
Jonatas Oliveira Silva <JonatasOliveira@ibm.com>
2025-04-29 13:18:06 -0300

    Implementando BD Oracle. Java 17 e Spring 3.1.6

--- pagamento/src/main/resources/data.sql
+++ pagamento/src/main/resources/data.sql
@@ -1,18 +1,34 @@
 INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
-VALUES 
-('Loja Tech', '11111111000101', 'lojatech@exemplo.com', 'senha123', 1000.00),
-('Loja Games', '22222222000102', 'lojagames@exemplo.com', 'senha123', 1500.00),
-('Loja Roupas', '33333333000103', 'lojaroupas@exemplo.com', 'senha123', 800.00),
-('Loja Pet', '44444444000104', 'lojapet@exemplo.com', 'senha123', 600.00),
-('Loja Café', '55555555000105', 'lojacafe@exemplo.com', 'senha123', 1200.00),
-('João Silva', '12345678900', 'joao@exemplo.com', 'senha123', 00.00),
-('Maria Oliveira', '23456789011', 'maria@exemplo.com', 'senha123', 750.00),
-('Carlos Souza', '34567890122', 'carlos@exemplo.com', 'senha123', 300.00),
-('Ana Lima', '45678901233', 'ana@exemplo.com', 'senha123', 1200.00),
-('Fernanda Costa', '56789012344', 'fernanda@exemplo.com', 'senha123', 950.00);
+VALUES ('Loja Tech', '11111111000101', 'lojatech@exemplo.com', 'senha123', 1000.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Loja Games', '22222222000102', 'lojagames@exemplo.com', 'senha123', 1500.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Loja Roupas', '33333333000103', 'lojaroupas@exemplo.com', 'senha123', 800.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Loja Pet', '44444444000104', 'lojapet@exemplo.com', 'senha123', 600.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Loja Café', '55555555000105', 'lojacafe@exemplo.com', 'senha123', 1200.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('João Silva', '12345678900', 'joao@exemplo.com', 'senha123', 00.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Maria Oliveira', '23456789011', 'maria@exemplo.com', 'senha123', 750.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Carlos Souza', '34567890122', 'carlos@exemplo.com', 'senha123', 300.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Ana Lima', '45678901233', 'ana@exemplo.com', 'senha123', 1200.00);
+INSERT INTO USUARIO (nome_Completo, cpf_cnpj, email, senha, saldo)
+VALUES('Fernanda Costa', '56789012344', 'fernanda@exemplo.com', 'senha123', 950.00);
 
 -- Inserir os IDs na tabela `Lojista`
-INSERT INTO lojista (id) VALUES (1), (2), (3), (4), (5);
+INSERT INTO lojista (id) VALUES (1);
+INSERT INTO lojista (id) VALUES (2);
+INSERT INTO lojista (id) VALUES (3); 
+INSERT INTO lojista (id) VALUES (4);
+INSERT INTO lojista (id) VALUES (5);
 
 -- Inserir os IDs na tabela `usuario_comum`
-INSERT INTO usuario_comum (id) VALUES (6), (7), (8), (9), (10);
+INSERT INTO usuario_comum (id) VALUES (6);
+INSERT INTO usuario_comum (id) VALUES (7);
+INSERT INTO usuario_comum (id) VALUES (8);
+INSERT INTO usuario_comum (id) VALUES (9);
+INSERT INTO usuario_comum (id) VALUES (10);
